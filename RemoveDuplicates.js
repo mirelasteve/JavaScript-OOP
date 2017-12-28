f2c49b56517a94cf38f3a8874fa457d70dbbc0bf
mirelasteve <mirelasteve@abv.bg>
2017-12-28 16:09:00 +0200

    Create RemoveDuplicates.js

---
+++ RemoveDuplicates.js
@@ -0,0 +1,18 @@
+ function ListNode(val) {
+     this.val = val;
+     this.next = null;
+  }
+const deleteDuplicates = (head)=> {
+     if(head === null || head.next ===null) {
+         return head;
+     }
+    let prime=head;
+       while(prime.next!=null){
+                if(prime.val==prime.next.val){
+            prime.next=prime.next.next;            
+                    } else {
+        prime=prime.next;
+                    }
+    }
+    return head;
+};
